83a0983872b47e3ae0430b1778ab4924a81f4321
overlookmotel <theoverlookmotel@gmail.com>
2020-04-19 15:36:30 +0100

    Tests: Import from package name [refactor]

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -3,5 +3,9 @@
 module.exports = {
 	extends: [
 		'@overlookmotel/eslint-config-jest'
-	]
+	],
+	rules: {
+		'import/no-unresolved': ['error', {ignore: ['^@overlook/{{name}}$']}],
+		'node/no-missing-require': ['error', {allowModules: ['@overlook/{{name}}']}]
+	}
 };
